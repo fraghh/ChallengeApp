df538a0cbfc5909db90b2f45e358d6c39c2f1b33
Michal Kaminski <mikaminski81@gmail.com>
2024-01-12 11:03:54 +0100

    nowy commit

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿var name = "ewa";
+var sex = "M";
+var age = 17;
+
+if (sex == "F" && age < 30)
+{
+    Console.WriteLine("kobieta lat 30");
+}
+else if (name == "ewa" && age == 30)
+{
+    Console.WriteLine("Ewa lat 30.");
+}
+else if(sex=="M" &&  age < 18)
+{
+    Console.WriteLine("niepelnoletni mezczyzna");
+}
+else 
+{
+    Console.WriteLine("nic");
+}
