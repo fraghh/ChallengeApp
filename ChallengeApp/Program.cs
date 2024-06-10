3a1422ff0518179650755ef31e5ae7d376fe6914
Michal Kaminski <mikaminski81@gmail.com>
2024-06-11 00:20:16 +0200

    zmienne

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,29 @@
-﻿var myAge = 33;
-var myAge2 = 33;
+﻿//liczby calkowite
+int myAge = 120;
+int myNum = myAge + 5;
+Console.WriteLine(myNum);
+int myVar = int.MinValue;
+uint myVar2 = uint.MinValue;
+long myVar3 = long.MaxValue;
+// liczby zmiennoprzecinkowe
+double myVar4 = double.MaxValue;
+decimal myVar5 = decimal.MaxValue;
+//zmienne tekstowe
+
+string name = "adam";
+string surname = "kamizel";
+
+string result = name +" "+ surname + myAge;
+
+Console.WriteLine(result);
+//zmienna char
+char myVar6= 'a';
+var result2=name.ToArray(); 
+
+//zmienna logiczna
+
+bool isActive= true;
+isActive= false;
+
+var isValid = 5 > 6;
+Console.WriteLine(isValid);
