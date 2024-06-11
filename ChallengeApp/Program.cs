a046cb1887ef8597f57a3d6f5425c5f369ae6569
Michal Kaminski <mikaminski81@gmail.com>
2024-06-11 23:21:18 +0200

    klasy full

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,4 +1,6 @@
-﻿User user1 = new User("adam","1");
+﻿using ChallengeApp;
+
+User user1 = new User("adam","1");
 User user2 = new User("tom", "1");
 User user3 = new User("rom", "1");
 
@@ -11,53 +13,7 @@ user1.AddScore(2);
 var result = user1.Result;
 
 Console.WriteLine(result);
-class User
-{
 
-
-    private List<int> score = new List<int>();
-    
-
-
-    public User(string login, string password)
-    {
-        this.Login = login;
-        this.Password = password;
-        this.Name = "-";
-        
-    }
-    
-    public string Login { get; private set; }
-    public string Password { get; private set; }
-    public string Name { get; private set; }
-
-    public int Result
-    {
-        get
-        {
-            return this.score.Sum();
-        }
-    }
-
-
-
-    public void AddScore(int number)
-    {
-        this.score.Add(number);
-    }
-    
-    
-    
-    
-    
-    
-  
-    
-        
-    
-    
-    
-    }
 
 
     
---
+++ ChallengeApp/User.cs
@@ -0,0 +1,51 @@
+﻿
+namespace ChallengeApp
+{
+    public class User
+    {
+        public static string GameName = "diablo";
+
+        private List<int> score = new List<int>();
+
+
+
+        public User(string login, string password)
+        {
+            this.Login = login;
+            this.Password = password;
+            this.Name = "-";
+
+        }
+
+        public string Login { get; private set; }
+        public string Password { get; private set; }
+        public string Name { get; private set; }
+
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+
+
+        public void AddScore(int number)
+        {
+            this.score.Add(number);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
