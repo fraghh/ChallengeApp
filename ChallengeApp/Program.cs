2dd378ce786061266ec840edb0c970419ff6805f
Michal Kaminski <mikaminski81@gmail.com>
2024-06-11 02:36:17 +0200

    Dzien 4 zadanie domowe

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,29 +1,17 @@
-﻿//liczby calkowite
-int myAge = 120;
-int myNum = myAge + 5;
-Console.WriteLine(myNum);
-int myVar = int.MinValue;
-uint myVar2 = uint.MinValue;
-long myVar3 = long.MaxValue;
-// liczby zmiennoprzecinkowe
-double myVar4 = double.MaxValue;
-decimal myVar5 = decimal.MaxValue;
-//zmienne tekstowe
+﻿string name = "wa";
+string sex = "m";
+int age = 43;
 
-string name = "adam";
-string surname = "kamizel";
 
-string result = name +" "+ surname + myAge;
-
-Console.WriteLine(result);
-//zmienna char
-char myVar6= 'a';
-var result2=name.ToArray(); 
-
-//zmienna logiczna
-
-bool isActive= true;
-isActive= false;
-
-var isValid = 5 > 6;
-Console.WriteLine(isValid);
+if (sex == "k" && age < 30) 
+{
+    Console.WriteLine("kbieta ponizej 30");
+}
+else if (name =="ewa"  &&  age == 30)
+{
+    Console.WriteLine("Ewa lat 30");
+}
+else if (sex == "m" && age < 18)
+{
+    Console.WriteLine("niepelnoletni mezczyzna");
+}
