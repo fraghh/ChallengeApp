dba4e1348a08b2503c8be14fb9aae3a7a3b3c4c6
Michal Kaminski <mikaminski81@gmail.com>
2024-06-11 17:54:51 +0200

    dzien 5 zadanie domowe

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,56 +1,23 @@
-﻿using System;
+﻿var digit = new int[10];
 
-int[] grades = new int[5];
+Console.Write("podaj liczbe: ");
+string number = Console.ReadLine();
 
-string[] daysOfWeeks = new string[7];
-
-daysOfWeeks[0] = "poniedzialek";
-daysOfWeeks[1] = "wtorek";
-daysOfWeeks[2] = "sroda";
-daysOfWeeks[3] = "czwartek";
-daysOfWeeks[4] = "piatek";
-daysOfWeeks[5] = "sobota";
-daysOfWeeks[6] = "niedziela";
-
-Console.WriteLine(daysOfWeeks[4]);
-
-string[] daysOfTheWeek = { "poniedzialek", "wtorek", "sroda", "czwartek", "piatek", "sobota", "niedziela" };
-
-Console.WriteLine(daysOfTheWeek[3]);
-
-for (int i=0; i <=1000;i++)
+foreach (char a in number)
 {
-    Console.WriteLine(i);
+    int cyfra = (int)char.GetNumericValue(a);
+
+    for (int i = 0; i < digit.Length; i++)
+    {
+        if (i == cyfra)
+        {
+            digit[i]++;
+        }
+    }
 }
-
-//int size = daysOfTheWeek.Length;
-
-for (var i = 0; i< daysOfTheWeek.Length; i++)
+Console.WriteLine("Liczba to " + number);
+for (int i = 0; i < (digit.Length); i++)
 {
-    Console.WriteLine(daysOfTheWeek[i]);
-}
 
-//List<string> daysOfWeek = new List<string>();
-
-//var daysOfWeek() = ( "poniedzialek", "wtorek", "sroda", "czwartek", "piatek", "sobota", "niedziela" );
-
-//for (var i = 0; i < daysOfWeek.Count; i++)
-//{
-/// Console.WriteLine(daysOfWeek[i]);
-//}
-
-List<string> daysOfWeek = new List<string>();
-
-daysOfWeek[0] = "poniedzialek";
-daysOfWeek[1] = "wtorek";
-daysOfWeek[2] = "sroda";
-daysOfWeek[3] = "czwartek";
-daysOfWeek[4] = "piatek";
-daysOfWeek[5] = "sobota";
-daysOfWeek[6] = "niedziela";
-
-
-foreach (var day in daysOfWeek)
-{
-    Console.WriteLine(day);
+    Console.WriteLine("Cyfra " + i + " wystapila " + digit[i] + " razy.");
 }
