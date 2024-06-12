7fb9d61a84424e6c2da03126374e2dcad362d90d
Michal Kaminski <mikaminski81@gmail.com>
2024-06-13 00:08:47 +0200

    zadanie domowe dzien 7

--- ChallengeApp.test/UnitTest1.cs
+++ ChallengeApp.test/UnitTest1.cs
@@ -5,24 +5,64 @@ namespace ChallengeApp.test
     public class Tests
     {
         [Test]
-        public void Setup()
+        public void Test1()
         {
             //arrange
-            var user = new User("adam", "1");
-            user.AddScore(5);
-            user.AddScore(6);
+            var employee = new Employee("adam", "1", 34);
+            employee.AddScore(5);
+            employee.AddScore(6);
 
 
             //act
 
-            var result = user.Result;
+            var result = employee.Result;
 
             //assert
 
             Assert.AreEqual(11, result);
 
         }
+        [Test]
+        public void Test2()
+        {
+            //arrange
+            var employee = new Employee("adam", "1", 34);
+            employee.AddScore(5);
+            employee.AddScore(6);
+            employee.AddScore(-6);
 
-        
+
+            //act
+
+            var result = employee.Result;
+
+            //assert
+
+            Assert.AreEqual(5, result);
+
+        }
+        [Test]
+        public void Test3()
+        {
+            //arrange
+            var employee = new Employee("adam", "1", 34);
+            employee.AddScore(5);
+            employee.AddScore(6);
+            employee.AddScore(-6);
+            employee.AddScore(-5);
+
+
+
+            //act
+
+            var result = employee.Result;
+
+            //assert
+
+            Assert.AreEqual(0, result);
+
+        }
+
+
     }
 }
